62111d3f751155b40f42f7066ed12d8cb89a7194
shinpei-nonaka <shinpei_nonaka@jrass.jp>
2022-10-19 16:02:26 +0900

    aaa

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by PC-SYSKAI556 on 2022/10/19.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
